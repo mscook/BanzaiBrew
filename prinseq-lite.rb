302430a40191361b1a918314a4f40718d3d05731
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2015-04-21 21:37:18 +1000

    PRINSEQ tweaks

--- prinseq-lite.rb
+++ prinseq-lite.rb
@@ -4,10 +4,18 @@ class PrinseqLite < Formula
   version "0.20.4"
   sha1 "b8560cdc059e9b4cbb1bab5142de29bde5d33f61"
 
+  patch do
+    url "https://raw.githubusercontent.com/mscook/homebrew-BanzaiNGS/master/patches/prinseq-lite-Declare-perl-env-properly.patch"
+    sha256 "1fd60e73ff635aa9748cd6135ea3bbcdf6ba7500"
+  end
+
   def install
-    system "chmod +x prinseq-graphs-noPCA.pl"  
-    system "chmod +x prinseq-graphs.pl"
-    system "chmod +x prinseq-lite.pl"
+    system "chmod +x *.pl"  
+    system "mkdir bin"
+    system "cp *.pl bin"
+    system "ln -s bin/prinseq-graphs-noPCA.pl bin/prinseq-graphs-noPCA"
+    system "ln -s bin/prinseq-graphs.pl bin/prinseq-graphs"
+    system "ln -s bin/prinseq-lite.pl bin/prinseq-lite"
     prefix.install Dir["*"]
   end
 
