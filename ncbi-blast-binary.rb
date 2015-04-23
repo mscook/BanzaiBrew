3c6ed4d61525a141e2da7abd7d0ed2b9dd05c429
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2015-04-23 12:22:11 +1000

    Missed a required Perl module

--- ncbi-blast-binary.rb
+++ ncbi-blast-binary.rb
@@ -4,6 +4,8 @@ class NcbiBlastBinary < Formula
   version "2.2.30"
   sha1 "2528f99daf6af16869d6eea669f7fce3f7671993"
 
+  depends_on "List::MoreUtils" => :perl 
+
   def install
     prefix.install Dir["*"]
   end
