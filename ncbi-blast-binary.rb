33ae6f3916f47bd5be68c15c1d468c3a62bf1057
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2015-04-23 13:59:11 +1000

    Hangon

--- ncbi-blast-binary.rb
+++ ncbi-blast-binary.rb
@@ -4,7 +4,7 @@ class NcbiBlastBinary < Formula
   version "2.2.30"
   sha1 "2528f99daf6af16869d6eea669f7fce3f7671993"
 
-  depends_on "List::MoreUtils" => :perl 
+  #depends_on "List::MoreUtils" => :perl 
 
   def install
     prefix.install Dir["*"]
