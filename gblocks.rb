b825fbaf206014236297f1e22a9ba1bc7d045829
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2015-04-24 16:20:36 +1000

    Gblocks

---
+++ gblocks.rb
@@ -0,0 +1,14 @@
+class Gblocks < Formula
+  homepage "http://molevol.cmima.csic.es/castresana/Gblocks.html"
+  version "0.91b"
+  url "http://molevol.cmima.csic.es/castresana/Gblocks/Gblocks_Linux64_0.91b.tar.Z"
+  sha1 "149d8ae44346eab8e39b71309aa8961f37ffde76"
+
+  def install
+      prefix.install Gblocks
+  end
+
+
+  test do
+    end
+end
