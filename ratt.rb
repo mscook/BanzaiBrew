d3841fab3d3e7f0dae960febbfcd35df8179862a
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2015-04-27 13:27:27 +1000

    RATT RATT RATTY

--- ratt.rb
+++ ratt.rb
@@ -5,6 +5,12 @@ class Ratt < Formula
   version "0.18.0"
   
   head 'https://svn.code.sf.net/p/ratt/code/'
+  
+  patch do
+    url "https://raw.githubusercontent.com/mscook/homebrew-BanzaiNGS/master/patches/prinseq-lite-Declare-perl-env-properly.patch"
+    sha256 "5ff4a5266f79d2b71168cb0ed8f22b8d3e55e6a10e56dbea7061421c747dc44d"
+  end
+
 
   depends_on "mummer"
 
