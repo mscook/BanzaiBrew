f1f66d3824802a4ab74b7403511733f9d52c3e03
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2015-04-27 13:16:01 +1000

    Initial formula

---
+++ ratt.rb
@@ -0,0 +1,14 @@
+class Ratt < Formula
+  homepage 'http://www.gpsbabel.org'
+  url "https://svn.code.sf.net/p/ratt/code/", :revision => "18"
+  # version will be SVN revision
+  version "0.18.0"
+  
+  head 'https://svn.code.sf.net/p/ratt/code/'
+
+  depends_on "mummer"
+
+  def install
+  end
+end
+
